3e743a57c324825e6cd8f0872969826bf70baccc
Austin <anorquist@mail.csuchico.edu>
2024-05-28 12:23:08 -0700

    prod build

---
+++ nb/_next/static/chunks/app/home/page-f47e77e3e92673da.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[951],{3129:function(n,e,u){Promise.resolve().then(u.t.bind(u,9439,23))},9439:function(){}},function(n){n.O(0,[385,971,23,744],function(){return n(n.s=3129)}),_N_E=n.O()}]);
